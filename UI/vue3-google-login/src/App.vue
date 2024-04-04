c46176d71e6bb2cc387985f2089fe9d97dfd6cce
yaroslav.hyrskiy@elyx.net <yaroslav.hyrskiy@elyx.net>
2024-04-04 21:55:19 +0300

    UI added

---
+++ UI/vue3-google-login/src/App.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { CallbackTypes } from "./plugin";
+const callback: CallbackTypes.CredentialCallback = (response) => {
+  // This callback will be triggered when the user selects or login to
+  // his Google account from the popup
+  console.log("Credential JWT string", response.credential);
+};
+</script>
+
+<template>
+  <GoogleLogin :callback="callback" />
+</template>
